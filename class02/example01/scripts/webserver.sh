954a49fd8aa7181a2ba1b41535bae6cce89d9d1a
cristiangarciavd <cristian.garcia.vd@gmail.com>
2022-09-20 21:34:53 -0300

    vagrant_task

--- class02/example01/scripts/webserver.sh
+++ class02/example01/scripts/webserver.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-apt-get install php -y
+echo "Provisioning virtual machine..."
 
+# update packages
+apt-get update
+
+echo "Installing Nginx"
+# nginx service: install and start
+apt-get install nginx -y
+service nginx start
